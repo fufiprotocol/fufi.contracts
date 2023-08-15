847a51b4d90a835f9d3f077281644db2fa8c3310
fufismartdev <galaxychainlabs@gmail.com>
2023-08-15 16:35:29 +0800

    use 0.8.9

--- contracts/fft_erc20.sol
+++ contracts/fft_erc20.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.17;
+pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts@4.8.2/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts@4.8.2/token/ERC20/extensions/ERC20Burnable.sol";
