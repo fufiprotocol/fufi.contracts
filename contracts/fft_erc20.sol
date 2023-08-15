4986e09ba3d5be5a0385d2351de914e35cc1f483
fufismartdev <galaxychainlabs@gmail.com>
2023-08-15 18:11:53 +0800

    use v4.9.3 for OZ

--- contracts/fft_erc20.sol
+++ contracts/fft_erc20.sol
@@ -1,11 +1,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
-import "@openzeppelin/contracts/security/Pausable.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts@4.9.3/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts@4.9.3/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts@4.9.3/token/ERC20/extensions/ERC20Capped.sol";
+import "@openzeppelin/contracts@4.9.3/security/Pausable.sol";
+import "@openzeppelin/contracts@4.9.3/access/Ownable.sol";
 
 
 /// @custom:security-contact fufidex@gmail.com
